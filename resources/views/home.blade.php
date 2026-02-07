bb45b9085e8e482e4b6ee74ffbc50e5f7dc199c0
antallarico <antoniotallarico78@gmail.com>
2026-02-07 17:54:36 +0100

    Delete resources directory

--- resources/views/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Laralife – Home')
-
-@section('content')
-    <h1 class="mb-4">Benvenuto in Laralife</h1>
-    <p>Qui potrai gestire tutti i moduli della tua vita personale.</p>
-    <p class="text-muted">Usa il menu a sinistra per accedere ai moduli.</p>
-@endsection
